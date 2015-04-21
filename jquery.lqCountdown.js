12e1ed2b36b0ae85ab72a9e0b4fbe9a8d01a7550
LinQuan <linquan@hexindai.com>
2015-04-21 18:30:17 +0800

    remove some useless code

--- jquery.lqCountdown.js
+++ jquery.lqCountdown.js
@@ -3,7 +3,7 @@
  * https://github.com/mlinquan/jQuery-lqCountdown-Plugin
  *
  * @version
- * 0.0.1 (April 16, 2015)
+ * 0.0.2 (April 16, 2015)
  *
  * @copyright
  * Copyright (C) 2015 LinQuan.
@@ -11,85 +11,10 @@
  * @license
  * Dual licensed under the MIT and GPL licenses.
  */
-if (!Date.format) {
-    Date.prototype.format = function(format) {
-        var o = {
-            "M+":this.getMonth() + (new Date("2015-01-01").getMonth() == 0 ? 1 :0),
-            "d+":this.getDate(),
-            "h+":this.getHours(),
-            "m+":this.getMinutes(),
-            "s+":this.getSeconds(),
-            "q+":Math.floor((this.getMonth() + 3) / 3),
-            S:this.getMilliseconds()
-        };
-        if (/(y+)/.test(format)) {
-            format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        }
-        for (var k in o) {
-            if (new RegExp("(" + k + ")").test(format)) {
-                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] :("00" + o[k]).substr(("" + o[k]).length));
-            }
-        }
-        return format;
-    };
-}
-
-if (!String.toDate) {
-    String.prototype.toDate = function(format) {
-        var Mon = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-        var dt = this.split(", ")[1].split(" ");
-        if (dt.length != 5) return;
-        var y = dt[2], M = Mon.indexOf(dt[1]) + 1, m = m < 10 ? "0" + M :M, d = dt[0], time = dt[3], tzone = dt[4];;
-        return new Date("" + y + "/" + M + "/" + d + " " + time + " " + tzone);
-    };
-}
-
-if (!Number.cover) {
-    Number.prototype.cover = function(n) {
+if (!Number.prototype.cover || !String.prototype.cover) {
+    Number.prototype.cover = String.prototype.cover = function(n) {
         var nl = ("" + this).length;
         return "" + new Array(n > nl ? n - nl + 1 :0).join(0) + "" + this;
-    };
-}
-
-if (!String.toDateFormat) {
-    String.prototype.toDateFormat = function(format) {
-        var ms = this;
-        if (!Number(ms) || ms > 365*24*60*60*1000) {
-            return;
-        }
-        if(!format) {
-            format = '{d} days, {h} hours, {m} minutes, {s} seconds and {S} milliseconds.';
-        }
-        var o = {
-            "{d+}": parseInt(ms / 1000 / 60 / 60 / 24, 10),
-            "{h+}": parseInt(ms / 1000 / 60 / 60 % 24, 10),
-            "{m+}": parseInt(ms / 1000 / 60 % 60, 10),
-            "{s+}": parseInt(ms / 1000 % 60, 10),
-            "{S+}": parseInt(ms % 1000)
-        };
-        for (var k in o) {
-            if (new RegExp("(" + k + ")").test(format)) {
-                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] :("00" + o[k]).substr(("" + o[k]).length));
-            }
-        }
-        return format;
-    }
-}
-
-if (!Array.indexOf) {
-    Array.prototype.indexOf = function(obj) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] == obj) {
-                return i;
-            }
-        }
-        return -1;
-    };
-}
-
-if (!Array.remove) {
-    Array.prototype.remove = function(i) {
-        this.splice(i, 1);
     };
 }
 
@@ -164,10 +89,10 @@ if (!Array.remove) {
                 global: false,
                 success:function(xml,status,xhr) {
                     var servertime = xhr.getResponseHeader("Date").toString();
-                    var r_servertime = servertime.toDate();
+                    //var r_servertime = servertime.toDate();
                     var now = new Date().getTime();
                     var st = new Date(servertime).getTime();
-                    $.lqCountdown.time_diff = (st - now);
+                    $.lqCountdown.time_diff = Math.ceil((st - now) / 1000)*1000;
                     $that.do_lqCountdown(opt);
                 },
                 beforeSend: function(xhr) {
